9aaff79f56ab119fbc00026c6e16b0da245f8c28
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2023-12-01 12:52:30 +0100

    update RegistrazioneAudio

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Immagine img = new Immagine("Parigi");
         Video vd = new Video("TimeLaps", 500);
-        RegistrazioneAudio audio = new RegistrazioneAudio("Discorso", 800);
+        RegistrazioneAudio audio = new RegistrazioneAudio("Discorso", 800, 0);
 
         img.play();
         vd.play();
+        audio.play();
+        audio.alzaVolume();
+        audio.play();
+        audio.alzaVolume();
+        audio.play();
+        audio.abbassaVolume();
         audio.play();
     }
 }
--- src/entities/RegistrazioneAudio.java
+++ src/entities/RegistrazioneAudio.java
@@ -1,10 +1,36 @@
 package entities;
 
 public class RegistrazioneAudio extends ElementoMultimediale{
-
-    public RegistrazioneAudio(String titolo, int durata) {
+    private int volume;
+    public RegistrazioneAudio(String titolo, int durata, int volume) {
         super(titolo, durata);
+        this.volume = volume;
     }
 
+    public int getVolume() {
+        return volume;
+    }
 
+    public  void abbassaVolume(){
+        if(volume > 0){
+            volume --;
+        }else {
+            System.out.println("Il volume è già impostato a zero!");
+        }
+    }
+    public  void alzaVolume(){
+            volume ++;
+    }
+
+    @Override
+    public void play() {
+            System.out.println("Riproduzione:" + getTitolo() + " (Durata: " + getDurata() + " secondi)");
+            System.out.println("Volume:");
+            for (int j = 0; j < volume; j++) {
+                for (int i = 0; i < volume; i++) {
+                    System.out.print("!");
+                }
+                System.out.println();
+            }
+    }
 }
