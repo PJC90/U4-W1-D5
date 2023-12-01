00b75be220b1ff74c5c13b177136f7e1f4a0795e
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2023-12-01 14:37:46 +0100

    Luminosità immagine

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ import entities.Video;
 
 public class Main {
     public static void main(String[] args) {
-        Immagine img = new Immagine("Parigi");
+        Immagine img = new Immagine("Parigi", 5);
         Video vd = new Video("TimeLaps", 2,1, 1);
         RegistrazioneAudio audio = new RegistrazioneAudio("Discorso", 2,5);
 
-        img.play();
+        img.show();
+        img.diminuisciLuminosita();
+        img.show();
         vd.play();
         vd.aumentaLuminosita();
         vd.play();
--- src/entities/Immagine.java
+++ src/entities/Immagine.java
@@ -1,10 +1,31 @@
 package entities;
 
 public class Immagine extends ElementoMultimediale{
-
-    public Immagine(String titolo) {
+    public int luminosita;
+    public Immagine(String titolo, int luminosita) {
         super(titolo);
+        this.luminosita = luminosita;
+    }
+    public void aumentaLuminosita(){
+        luminosita++;
+    }
+    public void diminuisciLuminosita(){
+        if(luminosita > 0) {
+            luminosita--;
+        }else{
+            System.out.println("la luminosità è già impostata a 0");
+        }
     }
 
 
+    public void show() {
+        System.out.println("Riproduzione:" + getTitolo() );
+        System.out.println("Luminosità"+ getTitolo());
+        for (int a = 0; a < luminosita; a++) {
+            for (int b = 0; b < luminosita; b++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
